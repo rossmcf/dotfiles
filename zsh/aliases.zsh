63cee45dd0900269b5b7e0338b0fb372f75a24d0
rossmcf <ross@rossmcf.com>
2016-04-06 11:57:20 +0100

    Add platform env aliases for zsh.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,4 @@
 alias reload!='. ~/.zshrc'
+alias @='open /Applications/Atom.app; atom '
+alias startenv='VBoxManage startvm platform-dev --type headless;sleep 5;ssh dev@commerce.mesh.mx.local'
+alias stopenv='VboxManage controlvm platform-dev acpipowerbutton'
